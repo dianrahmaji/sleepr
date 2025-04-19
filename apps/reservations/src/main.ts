9fe4cde65854c47c855dd2122308b4c44e684840
Dian Rahmaji <dianrahmaji@gmail.com>
2025-04-19 18:49:06 +0700

    feat(logger): create and implement `LoggerModule`

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
