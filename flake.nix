0113639d63bba1fa73f91c083332f52b1e2edebc
Dian Rahmaji <dianrahmaji@gmail.com>
2025-05-12 11:13:57 +0700

    chore: add gcloud nix package

--- flake.nix
+++ flake.nix
@@ -10,12 +10,18 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import (nixpkgs) { inherit system; });
+        gdk = pkgs.google-cloud-sdk.withExtraComponents (
+          with pkgs.google-cloud-sdk.components; [
+            gke-gcloud-auth-plugin
+          ]
+        );
       in {
         devShell = pkgs.mkShell {
 	        buildInputs = with pkgs; [
             nest-cli
             nodejs_22
             pnpm
+            gdk
           ];
         };
       }
